2ee8d2553ec309c420bcd866615d072324bd219b
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2021-08-26 14:29:46 +0700

    category constants

---
+++ src/constants/categories.js
@@ -0,0 +1,30 @@
+export const CATEGORIES = [
+  {
+    name: "Apple",
+    slug: "apple",
+  },
+  {
+    name: "Playstation",
+    slug: "playstation",
+  },
+  {
+    name: "Microsoft",
+    slug: "microsoft",
+  },
+  {
+    name: "Anime",
+    slug: "anime",
+  },
+  {
+    name: "Google",
+    slug: "google",
+  },
+  {
+    name: "Gaming",
+    slug: "gaming",
+  },
+  {
+    name: "React",
+    slug: "react",
+  },
+];
