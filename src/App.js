d61d6bce2d3f676ae9a43340ccd5c1f76c0d0439
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2021-08-26 10:39:13 +0700

    use service in app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useState, useEffect } from "react";
 import Container from "./components/Container";
 import Navbar from "./components/Navbar";
 import Loading from "./components/Loading";
 import Error from "./components/Error";
+import { getNews } from "./services/getNews";
 function App() {
+  useEffect(() => {
+    const fetchTechNews = async () => {
+      const res = await getNews({
+        searchQuery: "microsoft",
+      });
+      console.log(res);
+    };
+    fetchTechNews();
+  }, []);
   return (
     <>
       <Navbar />
