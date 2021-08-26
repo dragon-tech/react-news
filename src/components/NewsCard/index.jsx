c168e6c87a7a64f3203aa668e2a0e304c5e3305f
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2021-08-26 11:38:23 +0700

    news card component

---
+++ src/components/NewsCard/index.jsx
@@ -0,0 +1,60 @@
+import classnames from "classnames";
+import styles from "./NewsCard.module.css";
+import PropTypes from "prop-types";
+
+const NewsCard = ({
+  src,
+  title,
+  publishedAt,
+  author,
+  sourceName,
+  description,
+  url,
+  notLasChild,
+}) => {
+  return (
+    <>
+      <div
+        className={classnames(styles.newsCard, {
+          [styles.newsCardGap]: notLasChild,
+        })}
+      >
+        <div className={styles.imgContainer}>
+          <img
+            className={styles.img}
+            src={src}
+            alt={`${title} thumbnail img`}
+          />
+          <p className={styles.imgTitle}>{title}</p>
+        </div>
+
+        <div className={styles.newsCardContent}>
+          <p className={styles.newsCardDate}>{publishedAt}</p>
+          <p className={styles.newsCardAuthor}>{`${author} | ${sourceName}`}</p>
+          <p className={styles.newsCardDesc}>{description}</p>
+          <a
+            className={styles.url}
+            href={url}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Go to website
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+NewsCard.propTypes = {
+  src: PropTypes.string,
+  title: PropTypes.string,
+  publishedAt: PropTypes.string,
+  author: PropTypes.string,
+  sourceName: PropTypes.string,
+  description: PropTypes.string,
+  url: PropTypes.string,
+  notLasChild: PropTypes.bool,
+};
+
+export default NewsCard;
--- src/components/NewsList/index.jsx
+++ src/components/NewsList/index.jsx
@@ -1,14 +1,23 @@
 import PropTypes from "prop-types";
 import styles from "./NewsList.module.css";
+import NewsCard from "../NewsCard";
 
 const NewsList = ({ articles }) => {
   return (
     <div className={styles.newsList}>
       {articles.map((article, index, arr) => {
         return (
-          <div key={index}>
-            <h1>{article.title}</h1>
-          </div>
+          <NewsCard
+            key={index}
+            src={article.urlToImage}
+            title={article.title}
+            publishedAt={article.publishedAt}
+            author={article.author}
+            sourceName={article.source.name}
+            description={article.description}
+            url={article.url}
+            notLasChild={!(arr.length === index + 1)}
+          />
         );
       })}
     </div>
