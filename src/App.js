4467fe27f321b76abb4760cee442d032e9c2560b
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2021-08-26 09:30:33 +0700

    initial app components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Container from "./components/Container";
 import Navbar from "./components/Navbar";
+import Loading from "./components/Loading";
+import Error from "./components/Error";
 function App() {
   return (
     <>
       <Navbar />
-      <Container></Container>
+      <Container>
+        <h1>hello, world</h1>
+      </Container>
     </>
   );
 }
