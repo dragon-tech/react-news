7c856e7905a1de96c7853a1166082f284f68ddf3
dragon-tech <66893576+dragon-tech@users.noreply.github.com>
2021-08-26 13:59:20 +0700

    format date utils

--- src/components/NewsCard/index.jsx
+++ src/components/NewsCard/index.jsx
@@ -1,6 +1,7 @@
 import classnames from "classnames";
 import styles from "./NewsCard.module.css";
 import PropTypes from "prop-types";
+import { formatDate } from "../../utils/formatDate";
 
 const NewsCard = ({
   src,
@@ -29,7 +30,7 @@ const NewsCard = ({
         </div>
 
         <div className={styles.newsCardContent}>
-          <p className={styles.newsCardDate}>{publishedAt}</p>
+          <p className={styles.newsCardDate}>{formatDate(publishedAt)}</p>
           <p className={styles.newsCardAuthor}>{`${author} | ${sourceName}`}</p>
           <p className={styles.newsCardDesc}>{description}</p>
           <a
